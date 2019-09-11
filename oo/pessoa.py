c3f8788f6ab8b8b4dcd06e0ff9610b78529e473a
prportal <paulogportal@gmail.com>
2019-09-11 19:05:42 -0300

    Criado atributo de classe eyes and hands

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    eyes = 2
+    hands = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
@@ -12,7 +14,12 @@ if __name__ == '__main__':
     ##print(Pessoa.cumprimentar(p))
     ##print(id(p))
     ##print(Pessoa.cumprimentar(p), p.nome, p.idade)
+
     for filho in paulo.filhos:
         bnome = filho.nome
         bidade = filho.idade
         print(filho.nome, filho.idade)
+    paulo.olhos = 2
+    print(paulo.__dict__)
+    print(bruno.__dict__)
+    print(Pessoa.__dict__)
